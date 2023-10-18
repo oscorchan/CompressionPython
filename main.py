d03e4154c6e26302feecd28e7c106b85da6aed0b
oscorchan <oscar.cortijos@icloud.com>
2023-10-18 12:27:14 +0200

    etape 2

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
+chaine = "Ceci est un texte qui est un faux-texte"
+
+dico = {'texte': '1',
+ 'lorem': '2',
+ 'qui': '3',
+ 'donc': '4',
+ 'est': '5',
+ 'que': '6',
+ 'pour': '7',
+ 'ceci': '8',
+ 'faux-texte': '9',
+ 'dans': '10',
+ 'plus': '11',
+ 'avec': '12'}
+
 def decoupage(str):
     return str.split(' ')
     
 def reconstitution(liste):
     sep = " "
     return sep.join(liste)
-    
-    
-chaine = "Bonjour, je m'apelle oscar"
 
-print(decoupage(chaine))
+def compression(liste, dictionnaire):
+    result = []
+    for i in liste:
+        if (i in dictionnaire):
+            result.append(dictionnaire.get(i))
+        else:
+            result.append(i)
+            
+    return result
 
-print(reconstitution(decoupage(chaine)))
+print(compression(decoupage(chaine), dico))
