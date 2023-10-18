e8980b6241af15fde4bf05de21e4906936a6fd23
oscorchan <oscar.cortijos@icloud.com>
2023-10-18 13:55:32 +0200

    plein de bugs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 chaine = "Ceci est un texte qui est un faux-texte"
 texte2 = "Bonjour, je m'apelle oscar et j'aime beaucoup les chips. Les ananas sont délicieux. I ls cat"
 texte3 = "je ne veux remplacer que les mots de plus de 3 caractères qui apparaissent au moins 2 fois"
+texte4 = "Cette fonction vous permet de voir quels mots apparaissent le plus souvent dans le texte, et s'il est efficace de les remplacer par une référence : par exemple, remplacer le mot 'un' par la référence '254' n'entraîne pas de gain de place, mais au contraire une perte !À partir de vos observations, construisez un dictionnaire plus riche que celui proposé en exemple, tester votre compression / décompression et regardez le gain de place.Il vous est également possible de générer automatiquement un dictionnaire de références à des contraintes de votre choix, par exemple .Il vous faut pour cela une fonction E qui prend en paramètre un dictionnaire de nombre d'apparition des mots, et qui retourne un dictionnaire de référence."
 
 dico = {'texte': '1',
  'lorem': '2',
@@ -22,20 +23,22 @@ def reconstitution(liste):
     sep = " "
     return sep.join(liste)
 
-def compression(liste, dictionnaire):
+def assignation(liste, dictionnaire):
     result = []
     for i in liste:
         if (i in dictionnaire):
-            result.append(dictionnaire.get(i))
+            result.append(str(dictionnaire.get(i)))
         else:
             result.append(i)
             
     return result
 
-def decompression(liste, dictionnaire):
+def assignationInverse(liste, dictionnaire):
     dictionnaire = inverserDictionnaire(dictionnaire)
     result = []
+    print(dictionnaire)
     for i in liste:
+        print(i)
         if (i in dictionnaire):
             result.append(dictionnaire.get(i))
         else:
@@ -63,7 +66,7 @@ def creerDictionnaireAvance(liste):
     for i in liste:
         strN = str(n)
         if i not in dictionnaire and len(i) > len(strN):
-            dictionnaire[i] = n
+            dictionnaire[i] = strN
             n = n+1
             
     return dictionnaire
@@ -79,10 +82,23 @@ def creerDictionnaireSuperAvance(liste):
     
     return dictionnaire
 
-liste = decoupage(texte3)
+def compresser(texte):
+    liste = decoupage(texte)
+    dictionnaire = creerDictionnaireAvance(liste)
+    listeCompresse = assignation(liste, dictionnaire)
+    texteCompresse = reconstitution(listeCompresse)
+    
+    return texteCompresse, dictionnaire
 
-dicoSimple = creerDictionnaireBasique(liste)
-dicoAvance = creerDictionnaireAvance(liste)
-dicoSuperAvance = creerDictionnaireSuperAvance(liste)
+def decompresser(texte, dictionnaire):
+    print(dictionnaire)
+    liste = decoupage(texte)
+    listeDecompressee = assignationInverse(liste, dictionnaire)
+    texteDecompresse = reconstitution(listeDecompressee)
+    
+    print(texteDecompresse)
+    return texteDecompresse
 
-print(dicoSuperAvance)
+texteCompresse, dictionnaire = compresser(texte3)
+
+decompresser(texteCompresse, dictionnaire)
